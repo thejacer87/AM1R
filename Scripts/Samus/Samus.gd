822b1df0f45d85d3691603c87b9540f43bc7bbf9
Jace Bennest <jacebennest87@gmail.com>
2020-08-24 18:59:50 -0700

    wip

--- Scripts/Samus/Samus.gd
+++ Scripts/Samus/Samus.gd
@@ -16,8 +16,10 @@ var max_jump_height : float = 10 * Globals.UNIT_SIZE
 var min_jump_height : float = 1.25 * Globals.UNIT_SIZE
 var jump_duration := .5
 var velocity := Vector2.ZERO
+var input := Vector2.ZERO
 
 
+onready var label = $RichTextLabel
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
@@ -28,42 +30,22 @@ func _ready() -> void:
 	min_jump_velocity = -sqrt(2 * gravity * min_jump_height)
 
 
-func _apply_gravity(delta) -> void:
-	pass
-
-func run() -> void:
-	if is_on_floor():
-		speed = walk_speed
-
-func _handle_move_input -> void:
-	pass
-
-func move(delta: float) -> void:
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	input_vector = input_vector.normalized()
-
-	if input_vector != Vector2.ZERO:
-		animation_tree.set("parameters/Idle/blend_position", input_vector)
-		animation_tree.set("parameters/Run/blend_position", input_vector)
-		animation_state.travel("Run")
-		velocity = velocity.move_toward(input_vector * speed, ACC * delta)
-	else:
-		animation_state.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, delta)
+func _apply_gravity(delta: float) -> void:
+	velocity.y = velocity.y + gravity * delta if not is_on_floor() else 0
+	print(velocity.y)
+	print(is_on_floor())
 
 
-#	var snap = Vector2.DOWN * 8 if not is_jumping else Vector2.ZERO
-	velocity.y = min(velocity.y + gravity * delta, MAX_FALL_SPEED)
+func _handle_move_input() -> void:
+	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input = input.normalized()
+
+	velocity.x = input.x * speed
+
+
+func _apply_movement(delta: float) -> void:
 	var snap = Vector2.ZERO
-	move_and_collide(velocity)
-#	velocity = move_and_slide_with_snap(velocity, snap, FLOOR, true)
-#	velocity = move_and_slide(velocity, FLOOR, true)
-
-func jump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animation_state.travel("Idle")
-		velocity.y = max_jump_velocity
-	if Input.is_action_just_released("jump") and velocity.y < min_jump_velocity:
-		velocity.y = min_jump_velocity
+#	move_and_collide(velocity)
+	velocity = move_and_slide_with_snap(velocity, snap, FLOOR)
+#	velocity = move_and_slide(velocity, FLOOR)
--- Scripts/Samus/SamusStateMachine.gd
+++ Scripts/Samus/SamusStateMachine.gd
@@ -5,54 +5,58 @@ func _ready() -> void:
 	add_state("idle")
 	add_state("run")
 	add_state("shoot")
-	add_state("jumping")
+	add_state("jump")
+	add_state("fall")
 	add_state("wall_cling")
 	call_deferred("set_state", states.idle)
 
 
-func _input(event) -> void:
-	if [states.idle, states.run].has(state):
-		var input = Vector2.ZERO
-		input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-		input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-		input = input.normalized()
-
-		if input != Vector2.ZERO:
-			parent.animation_tree.set("parameters/Idle/blend_position", input)
-			parent.animation_tree.set("parameters/Run/blend_position", input)
-			parent.animation_state.travel("Run")
-#			parent.velocity = parent.velocity.move_toward(input * parent.speed, parent.ACC * delta)
-		else:
-			parent.animation_state.travel("Idle")
-			parent.velocity = Vector2.ZERO
-
-
-func _state_logic(delta) -> void:
+func _state_logic(delta: float) -> void:
 	parent._handle_move_input()
 	parent._apply_gravity(delta)
-	parent.run()
-	parent.jump()
-	parent.move(delta)
+	parent._apply_movement(delta)
 
 
-func _get_transition(_delta) -> String:
+func _input(event: InputEvent) -> void:
+	if [states.idle, states.run].has(state):
+		if event.is_action_pressed("jump"):
+			parent.velocity.y = parent.max_jump_velocity
+
+
+	if [states.jump].has(state):
+		if event.is_action_released("jump") && parent.velocity.y < parent.min_jump_velocity:
+			parent.velocity.y = parent.min_jump_velocity
+
+
+func _get_transition(delta: float):
 	match state:
 		states.idle:
 			if !parent.is_on_floor():
 				if parent.velocity.y < 0:
 					return states.jump
+				elif parent.velocity.y > 0:
+					return states.fall
 			elif parent.velocity.x != 0:
 				return states.run
 		states.run:
 			if !parent.is_on_floor():
 				if parent.velocity.y < 0:
 					return states.jump
+				elif parent.velocity.y > 0:
+					return states.fall
 			elif parent.velocity.x == 0:
 				return states.idle
 		states.jump:
 			if parent.is_on_floor():
 				return states.idle
-	return ""
+			elif parent.velocity.y >= 0:
+				return states.fall
+		states.fall:
+			if parent.is_on_floor():
+				return states.idle
+			elif parent.velocity.y < 0:
+				return states.jump
+	return null
 
 func _enter_state(new_state, old_state) -> void:
 	match new_state:
@@ -65,6 +69,10 @@ func _enter_state(new_state, old_state) -> void:
 		states.jump:
 			parent.animation_tree.set("parameters/Jump/blend_position", parent.input)
 			parent.animation_state.travel("Jump")
+		states.fall:
+			parent.animation_tree.set("parameters/Idle/blend_position", parent.input)
+			parent.animation_state.travel("Idle")
+	parent.label.text = String(state)
 
 
 func _exit_state(old_state, new_state) -> void:
