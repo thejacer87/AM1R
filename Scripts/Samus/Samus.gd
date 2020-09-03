c7bd085a02468c260c3324cf92636d93496012f6
Jace Bennest <jacebennest87@gmail.com>
2020-09-02 22:56:47 -0700

    turning wip

--- Scripts/Samus/Samus.gd
+++ Scripts/Samus/Samus.gd
@@ -10,4 +10,5 @@ onready var mode_label = $mode
 func _physics_process(delta):
 	label.text = $MotionStateMachine.current_state.get_name()
 	mode_label.text = $MorphStateMachine.current_state.get_name()
+	print(label.text)
 
