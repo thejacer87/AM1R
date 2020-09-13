1adb90d9a4346520bd66f6ecce43a8437c27932c
Jace Bennest <jace@silvertipsoftware.com>
2020-09-13 15:04:32 -0700

    beam stops on walls

--- Scripts/BeamCannon/Beam.gd
+++ Scripts/BeamCannon/Beam.gd
@@ -27,5 +27,9 @@ func _on_Timer_timeout():
 	queue_free()
 
 
-func _on_Beam_body_entered(body):
+func _on_Hitbox_body_entered(body):
+	queue_free()
+
+
+func _on_Hitbox_area_entered(area):
 	queue_free()
