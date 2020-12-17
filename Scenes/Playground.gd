f63397f0005bacb7957bdccdc135d69634b9810d
Jace Bennest <jacebennest87@gmail.com>
2020-12-16 18:57:31 -0800

    wip

---
+++ Scenes/Playground.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+func _ready() -> void:
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return # Error! We don't have a save to load.
+
+	# We need to revert the game state so we're not cloning objects
+	# during loading. This will vary wildly depending on the needs of a
+	# project, so take care with this step.
+	# For our example, we will accomplish this by deleting saveable objects.
+	var save_nodes = get_tree().get_nodes_in_group("persist")
+	for i in save_nodes:
+		i.queue_free()
+
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	save_game.open("user://savegame.save", File.READ)
+	while save_game.get_position() < save_game.get_len():
+		# Get the saved dictionary from the next line in the save file
+		var node_data = parse_json(save_game.get_line())
+
+		# Firstly, we need to create the object and add it to the tree and set its position.
+		var new_object = load(node_data["filename"]).instance()
+		get_node(node_data["parent"]).add_child(new_object)
+		new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
+
+		# Now we set the remaining variables.
+		for i in node_data.keys():
+			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+				continue
+			new_object.set(i, node_data[i])
+
+	save_game.close()
--- Scripts/Samus/Samus.gd
+++ Scripts/Samus/Samus.gd
@@ -10,6 +10,9 @@ onready var animation_tree = $AnimationTree
 onready var motion_state_machine = $MotionStateMachine
 onready var morph_state_machine = $MorphStateMachine
 
+func _ready() -> void:
+	var camera = load("res://Camera.tscn")
+	add_child(camera.instance())
 
 func _physics_process(_delta):
 	label.text = motion_state_machine.current_state.get_name()
@@ -18,3 +21,13 @@ func _physics_process(_delta):
 func bomb_jump() -> void:
 	if morph_state_machine.current_state == Globals.STATES["Morph"].states_map["morph_ball"]:
 		motion_state_machine._change_state("bomb_jump")
+
+func save(load_position) -> Dictionary:
+	var save = {
+		"filename": get_filename(),
+		"parent": get_parent().get_path(),
+		"pos_x": load_position.x,
+		"pos_y": load_position.y,
+		"camera": $Camera2D
+	}
+	return save
