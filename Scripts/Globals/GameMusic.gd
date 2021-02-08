09c51546f1d71f5e1ab0f0927ac4ed13ed8eb48b
Jace Bennest <jacebennest87@gmail.com>
2021-02-08 00:44:22 -0800

    add music

---
+++ Scripts/Globals/GameMusic.gd
@@ -0,0 +1,22 @@
+extends Node
+
+onready var audio := $AudioStreamPlayer
+
+
+func _ready() -> void:
+	Globals.GameMusic = self
+
+
+func update_volume(vol: int) -> void:
+	audio.volume_db = vol
+
+
+func play(path) -> void:
+	var audio_file = load(path)
+	audio.stop()
+	audio.stream = audio_file
+	audio.play()
+
+
+func stop() -> void:
+	audio.stop()
