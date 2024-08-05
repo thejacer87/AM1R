d39bb7513424474def428b1baee9046442ac2920
Jace Bennest <jacebennest87@gmail.com>
2024-08-04 20:58:32 -0700

    wip4

--- src/Main.gd
+++ src/Main.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var samus: Samus = $Samus
 @onready var label: Label = $Label
+@onready var input_label: Label = $InputLabel
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	input_label.text = "Input: " + str(Input.get_joy_name(0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- src/entities/characters/samus/Samus.gd
+++ src/entities/characters/samus/Samus.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 
 enum facing {LEFT, RIGHT}
 
-const SPEED := 150.0
-const JUMP_VELOCITY := -250.0
+const SPEED := 200.0
+const JUMP_VELOCITY := -275.0
 
 
 @export var energy := 99
@@ -15,30 +15,44 @@ const JUMP_VELOCITY := -250.0
 var looking := facing.RIGHT
 
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("left", "right")
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("left", "right")
 		
 	if direction:
-		velocity.x = direction * SPEED
+		if not is_on_floor():
+			velocity.x = direction * SPEED * 0.666667
+		else:
+			velocity.x = direction * SPEED
+			
 		if direction > 0.0:
-			animated_sprite_2d.play("run_right")
+			animated_sprite_2d.flip_h = false
+			if not is_on_floor():
+				animated_sprite_2d.play("jump_spin_right")
+			else:
+				animated_sprite_2d.play("run_right")
 			looking = facing.RIGHT
 		else:
-			animated_sprite_2d.play("run_left")
+			animated_sprite_2d.flip_h = true
+			if not is_on_floor():
+				animated_sprite_2d.play("jump_spin_left")
+			else:
+				animated_sprite_2d.play("run_left")
 			looking = facing.LEFT
 	elif is_on_floor():
 		if looking == facing.LEFT:
 			animated_sprite_2d.play("idle_left")
 		else:
+			animated_sprite_2d.flip_h = true
+			animated_sprite_2d.flip_h = false
 			animated_sprite_2d.play("idle_right")
 		velocity.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, 0)
+		velocity.x = move_toward(velocity.x, 0, 5)
 
 	move_and_slide()
 
