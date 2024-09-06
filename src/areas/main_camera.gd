32cd018b87b94ec22204d361dfec1b29cb5683b4
Jace Bennest <jacebennest87@gmail.com>
2024-09-05 22:53:07 -0700

    screen size locked in

--- src/areas/main_camera.gd
+++ src/areas/main_camera.gd
@@ -7,7 +7,7 @@ func set_camera_bounds(room: Room) -> void:
 	var camera_bounds := room.camera_bounds.get_children()
 
 	limit_top = (camera_bounds[Bounds.TOP] as Marker2D).global_position.y
-	limit_right = (camera_bounds[Bounds.RIGHT] as Marker2D).global_position.x
 	limit_bottom = (camera_bounds[Bounds.BOTTOM] as Marker2D).global_position.y
 	limit_left = (camera_bounds[Bounds.LEFT] as Marker2D).global_position.x
+	limit_right = (camera_bounds[Bounds.RIGHT] as Marker2D).global_position.x
 	position = Vector2.ZERO
